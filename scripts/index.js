d44653e8f9d82990c9570592a9bad217cae79187
Evgenii Ryvkin <antagonistjek91@gmail.com>
2022-12-12 02:40:19 +0300

    deleting and adding search results

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,49 @@
-import { debounce, ucFirst, sendRequest } from './functions.js';
-import { inputHandler } from './handlers.js';
+import { debounce } from './functions.js';
+import {
+  inputHandler,
+  searchListHandler,
+  deleteButtonHandler,
+} from './handlers.js';
 
 console.log('hi');
 
 const reposArr = [];
 const resultArr = [];
 
+const app = document.querySelector('.app');
 const searchInput = document.querySelector('.search__input');
 const datalist = document.querySelector('.search_list');
+const resultList = document.querySelector('.result');
 
 const inputHandlerDebo = debounce(inputHandler, 3000);
+
 //----------------------------------
 
-searchInput.addEventListener('input', () => {
+searchInput.addEventListener('input', (e) => {
   inputHandlerDebo(searchInput, datalist, reposArr);
 });
 
-searchInput.addEventListener('change', (e) => {});
+app.addEventListener('click', (e) => {
+  const autocomplit = e.target.closest('.search__autocomplit');
+  if (!!autocomplit) {
+    searchListHandler(
+      autocomplit,
+      resultList,
+      reposArr,
+      resultArr,
+      searchInput,
+      datalist
+    );
+    return;
+  }
 
+  const deleteButton = e.target.closest('.result__delete');
+  if (!!deleteButton) {
+    deleteButtonHandler(deleteButton, resultList, resultArr);
+    return;
+  }
+});
+
+searchInput.addEventListener('change', (e) => {});
 searchInput.addEventListener('focus', () => {});
 searchInput.addEventListener('blur', () => {});
--- scripts/render.js
+++ scripts/render.js
@@ -1,9 +1,8 @@
 import { ucFirst } from './functions.js';
 
-export { renderDatalist };
+export { renderDatalist, renderResultList };
 
 function renderDatalist(datalist, data) {
-  console.log('рендерится список автокомплита');
   while (datalist.firstChild) {
     datalist.removeChild(datalist.firstChild);
   }
@@ -19,5 +18,82 @@ function renderDatalist(datalist, data) {
     optionClon.textContent = ucFirst(elem.name);
     optionClon.dataset.id = id;
     datalist.append(optionClon);
+  });
+}
+
+function renderResultList(resultList, data) {
+  while (resultList.firstChild) {
+    resultList.removeChild(resultList.firstChild);
+  }
+  const resultFragment = document.createElement('div');
+  resultFragment.classList.add('result__elem');
+
+  const info = document.createElement('div');
+  info.classList.add('result__info');
+
+  const name = document.createElement('div');
+  name.classList.add('result__name');
+  let resultKey = document.createElement('span');
+  resultKey.classList.add('result__key');
+  resultKey.textContent = 'Name: ';
+  let resultValue = document.createElement('span');
+  resultValue.classList.add('result__value');
+  name.append(resultKey);
+  name.append(resultValue);
+
+  const owner = document.createElement('div');
+  owner.classList.add('result__owner');
+  resultKey = document.createElement('span');
+  resultKey.classList.add('result__key');
+  resultKey.textContent = 'Owner: ';
+  resultValue = document.createElement('span');
+  resultValue.classList.add('result__value');
+  owner.append(resultKey);
+  owner.append(resultValue);
+
+  const star = document.createElement('div');
+  star.classList.add('result__star');
+  resultKey = document.createElement('span');
+  resultKey.classList.add('result__key');
+  resultKey.textContent = 'Stars: ';
+  resultValue = document.createElement('span');
+  resultValue.classList.add('result__value');
+  star.append(resultKey);
+  star.append(resultValue);
+
+  info.append(name);
+  info.append(owner);
+  info.append(star);
+
+  const resultButtons = document.createElement('div');
+  resultButtons.classList.add('result__buttons');
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('result__delete');
+  resultButtons.append(deleteButton);
+
+  resultFragment.append(info);
+  resultFragment.append(resultButtons);
+
+  if (data.length > 0) {
+    resultList.hidden = false;
+  } else {
+    resultList.hidden = true;
+  }
+
+  data.forEach((obj, id) => {
+    let elemResult = resultFragment.cloneNode(true);
+    resultList.append(elemResult);
+
+    elemResult
+      .querySelector('.result__name')
+      .querySelector('.result__value').textContent = obj.name;
+    elemResult
+      .querySelector('.result__owner')
+      .querySelector('.result__value').textContent = obj.owner.login;
+    elemResult
+      .querySelector('.result__star')
+      .querySelector('.result__value').textContent = obj.stargazers_count;
+
+    elemResult.dataset.deleteId = id;
   });
 }
